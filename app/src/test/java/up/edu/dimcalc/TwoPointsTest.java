12cbae6ef9c7952f35d0ebf86b1d4281bbfc3793
JasonTwigg <jasontwigg1@gmail.com>
2018-03-22 16:29:57 -0700

    Initial commit

---
+++ app/src/test/java/up/edu/dimcalc/TwoPointsTest.java
@@ -0,0 +1,44 @@
+package up.edu.dimcalc;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by Jason on 3/22/2018.
+ */
+public class TwoPointsTest {
+    @org.junit.Test
+    public void getPoint() throws Exception {
+
+    }
+
+    @org.junit.Test
+    public void setPoint() throws Exception {
+
+    }
+
+    @org.junit.Test
+    public void randomValue() throws Exception {
+
+    }
+
+    @org.junit.Test
+    public void setOrigin() throws Exception {
+
+    }
+
+    @org.junit.Test
+    public void copy() throws Exception {
+
+    }
+
+    @org.junit.Test
+    public void distance() throws Exception {
+
+    }
+
+    @org.junit.Test
+    public void slope() throws Exception {
+
+    }
+
+}
