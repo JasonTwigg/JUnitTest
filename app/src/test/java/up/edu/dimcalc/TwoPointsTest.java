a1b266e99a86debe63d79d458137bf74fbbace97
JasonTwigg <jasontwigg1@gmail.com>
2018-03-22 16:37:31 -0700

    HI JOSH

--- app/src/test/java/up/edu/dimcalc/TwoPointsTest.java
+++ app/src/test/java/up/edu/dimcalc/TwoPointsTest.java
@@ -1,5 +1,7 @@
 package up.edu.dimcalc;
 
+import android.graphics.Point;
+
 import static org.junit.Assert.*;
 
 /**
@@ -8,16 +10,37 @@ import static org.junit.Assert.*;
 public class TwoPointsTest {
     @org.junit.Test
     public void getPoint() throws Exception {
+        TwoPoints testPoints = new TwoPoints();
+        Point p1 = testPoints.getPoint(0);
+        Point p2 = testPoints.getPoint(1);
+        assertEquals(p1.x, 0);
+        assertEquals(p1.y, 0);
+        assertEquals(p2.x, 0);
+        assertEquals(p2.y, 0);
 
     }
 
     @org.junit.Test
     public void setPoint() throws Exception {
+        TwoPoints testPoints = new TwoPoints();
+        testPoints.setPoint(0, 5, -3);
+        testPoints.setPoint(1, -3, 5);
+        Point p1 = testPoints.getPoint(0);
+        Point p2 = testPoints.getPoint(1);
+        assertEquals(p1.x, 5);
+        assertEquals(p1.y, -3);
+        assertEquals(p2.x, -3);
+        assertEquals(p2.y, 5);
 
     }
 
     @org.junit.Test
     public void randomValue() throws Exception {
+        //assertEquals(1,2);
+
+        TwoPoints testPoints = new TwoPoints();
+        testPoints.randomValue(0);
+        testPoints.randomValue(1);
 
     }
 
